e8da2fd9e739d4e7359ba732b519049992b4c72b
Terry Heath <theath@us.ibm.com>
2020-01-23 16:12:14 -0500

    another commit3

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ def main():
   spark = SparkSession(sc)
   print ("What the heck")
   print( "Hello World!" )
-  peopleDF = spark.read.json("examples/src/main/resources/people.json")
+  peopleDF = spark.read.json("/opt/spark/examples/src/main/resources/people.json")
 
   # DataFrames can be saved as Parquet files, maintaining the schema information.
   peopleDF.write.parquet("people.parquet")
